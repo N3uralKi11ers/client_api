a230549e040c6d9e5a23065902dbe2ecbe8f896b
NikolaySimakov <n.simakoff@gmail.com>
2023-08-29 18:43:21 +0300

    Add get transactions & get products histories methods

--- api/routes/history.py
+++ api/routes/history.py
@@ -1,20 +1,50 @@
 from fastapi import APIRouter
-from typing import Optional
+from typing import Optional, List
+from schemas import TransactionBase, ProductBase
 
 router = APIRouter()
 
 fake_db = [
-    
+
 ]
 
 @router.get(
-    '/',
-    name="All ",
+    '/{consumer_name}/',
+    name="All transactions",
     description="""
     This method returns all goods. 
-    """
+    """,
+    response_model=List[TransactionBase],
 )
-def get_history(
+def get_transactions_history(
+    consumer_name: str,
     limit: Optional[int],
 ):
-    return {"qwerty" : 12345}
+    return [
+        TransactionBase(
+            recipient="qwerty",
+            description="lrhlw lhrewlh wel",
+            amount=123.123,
+        ),
+        TransactionBase(
+            recipient="qwerty",
+            description="lrhlw lhrewlh wel",
+            amount=123.123,
+        ),
+        TransactionBase(
+            recipient="qwerty",
+            description="lrhlw lhrewlh wel",
+            amount=123.123,
+        )
+    ]
+
+
+@router.get(
+    '/{consumer_name}/products',
+    name="All products",
+    response_model=List[ProductBase],
+)
+def get_products_history(
+
+):
+    return []
